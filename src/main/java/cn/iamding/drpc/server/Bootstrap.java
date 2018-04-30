21a37596e514182511a0d5e4e714a4051884f9c8
XuDing <ahtcfg24@163.com>
2018-04-30 22:18:56 +0800

    完成服务端

--- src/main/java/cn/iamding/drpc/server/Bootstrap.java
+++ src/main/java/cn/iamding/drpc/server/Bootstrap.java
@@ -3,9 +3,6 @@ package cn.iamding.drpc.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by FrankChow on 2017/5/11.
- */
 @SpringBootApplication
 public class Bootstrap {
 
