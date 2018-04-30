1235d474068c541287709f0db634596fa08055d3
XuDing <ahtcfg24@163.com>
2018-04-30 22:18:19 +0800

    完成服务端

--- src/main/java/cn/iamding/drpc/server/listener/StartupListener.java
+++ src/main/java/cn/iamding/drpc/server/listener/StartupListener.java
@@ -6,9 +6,6 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-/**
- *
- */
 @Component
 public class StartupListener implements ApplicationListener<ContextRefreshedEvent> {
     @Autowired
